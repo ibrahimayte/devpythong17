d3cde33d7609c327ae0388adbba5bd2c1aa6997f
ibrahimayte <103316058+ibrahimayte@users.noreply.github.com>
2022-04-09 12:10:36 +0000

    Create commentaires_notes.py

---
+++ commentaires_notes.py
@@ -0,0 +1,19 @@
+# Commentaires de notes
+
+essayez :
+    note  =  int ( input ( "Saisissez une note : " ))
+sauf  ValueError :
+    print ( "Vous devez saisir un nombre !" )
+   
+si  note  <  10 :
+    impression ( "Médiocre" )
+si  note  >=  10  et  note  <=  11 :
+    imprimer ( "Passable" )
+si  note  >=  12  et  note  <=  13 :
+    estampe ( "Assez bien" )
+si  note  >=  14  et  note  <=  15 :
+    imprimer ( "Bien" )
+si  note  >=  16  et  note  <=  17 :
+    estampe ( "Très bien" )
+si  note  >=  18 :
+    impression ( "Excellente" )
